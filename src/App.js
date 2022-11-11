241e6a0f1e1692b45fd19299efca65540dd68f51
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-11-11 19:06:35 -0300

    estruturando o projeto, aula #18

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// tem que substituir o Switch por Routes, no react v6
+
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">NewProject</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+
+          <Route exact path="/company" element={<Company />} />
+
+          <Route exact path="/contact" element={<Contact />} />
+
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
