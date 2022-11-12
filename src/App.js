a7d38c515df04db1ebf918bf72511eaab9686955
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-11-12 15:10:44 -0300

    navbar , aula #19

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // tem que substituir o Switch por Routes, no react v6
 
 import Home from "./components/pages/Home";
@@ -7,27 +7,22 @@ import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
 import Container from "./components/layout/Container";
 
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/newproject">NewProject</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home />} />
-
           <Route exact path="/company" element={<Company />} />
-
           <Route exact path="/contact" element={<Contact />} />
-
           <Route exact path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
