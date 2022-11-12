c80043f85cc17a7f135e3d807e5ab33354f6b324
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-11-12 15:51:23 -0300

    footer , aula #19

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -11,16 +11,19 @@ function Navbar() {
           <img src={logo} alt="Costs" />
         </Link>
         <ul className={styles.list}>
-          <li class={styles.item}>
+          <li className={styles.item}>
             <Link to="/">Home</Link>
           </li>
-          <li class={styles.item}>
+          <li className={styles.item}>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li className={styles.item}>
             <Link to="/contact">Contact</Link>
           </li>
-          <li class={styles.item}>
+          <li className={styles.item}>
             <Link to="/company">Company</Link>
           </li>
-          <li class={styles.item}>
+          <li className={styles.item}>
             <Link to="/newproject">NewProject</Link>
           </li>
         </ul>
