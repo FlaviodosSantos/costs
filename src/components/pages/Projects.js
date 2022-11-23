cc9209d28e9c9c485b3a88237e35a102f9fae6f3
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-11-23 15:00:18 -0300

    removendo projetos, até 4m31s, aula #29

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,7 +4,9 @@ import styles from "./Projects.module.css";
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
 import ProjectCard from "../project/ProjectCard";
+
 import { useState, useEffect } from "react";
+
 import Loading from "../layout/Loading";
 
 function Projects() {
@@ -35,6 +37,21 @@ function Projects() {
     }, 3000);
   }, []);
 
+  function removeProject(id) {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjests(projects.filter((project) => project.id !== id));
+        //message
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
@@ -51,6 +68,7 @@ function Projects() {
               budget={project.budget}
               category={project.category.name}
               key={project.id}
+              handleRemove={removeProject}
             />
           ))}
         {!removeLoading && <Loading />}
