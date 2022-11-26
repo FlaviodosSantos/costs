50778b47adba76bd808d874ec6b981bf8d794515
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-11-26 17:05:44 -0300

    loading no editar , 2m01s, aula #31

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,25 +3,29 @@ import styles from "./Project.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
+import Loading from "../layout/Loading";
+
 function Project() {
   const { id } = useParams();
   const [project, setProject] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setProject(data);
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch((err) => console.log);
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProject(data);
+        })
+        .catch((err) => console.log);
+    }, 500);
   }, [id]);
 
-  return <p>{project.name}</p>;
+  return <>{project.name ? <p>{project.name}</p> : <Loading />}</>;
 }
 
 export default Project;
